ac3db7769f9d86f400e81d2cdf29c0847f40317c
Laurens Vijnck <laurens@everycure.org>
2026-02-05 09:07:00 +0100

    validate machine configurations in test

--- argo-kedro/tests/cli/test_cli.py
+++ argo-kedro/tests/cli/test_cli.py
@@ -1,26 +1,40 @@
 import pytest
 
 from kedro.pipeline import Pipeline, node
-from argo_kedro.pipeline import FusedPipeline
-from argo_kedro.framework.cli.cli import get_argo_dag
+from argo_kedro.pipeline import FusedPipeline, ArgoNode
+from argo_kedro.framework.cli.cli import get_argo_dag, MachineType
+
+@pytest.fixture
+def machine_types() -> dict[str, MachineType]:
+    return {
+        "default": MachineType(mem=16, cpu=2, num_gpu=0),
+        "n1-standard-4": MachineType(mem=16, cpu=4, num_gpu=0),
+        "n1-standard-8": MachineType(mem=16, cpu=8, num_gpu=0),
+    }
+
+@pytest.fixture
+def default_machine_type() -> str:
+    return "default"
 
 @pytest.fixture
 def pipeline() -> Pipeline:
     return Pipeline(
         [
-            node(
+            ArgoNode(
                 func=lambda x: x,
                 inputs="raw_data",
                 outputs="data",
                 tags=["preprocessing"],
                 name="preprocess_fun",
+                machine_type="n1-standard-4",
             ),
-            node(
+            ArgoNode(
                 func=lambda x: x,
                 inputs="data",
                 outputs="model",
                 tags=["training"],
                 name="train_fun",
+                machine_type="n1-standard-8",
             ),
         ]
     )
@@ -54,6 +68,7 @@ def fused_pipeline() -> Pipeline:
                     ),
                 ],
                 name="fused_modelling",
+                machine_type="n1-standard-8",
             ),
         ]
     )
@@ -95,25 +110,32 @@ def fused_pipeline_complex() -> Pipeline:
                     ),
                 ],
                 name="fused_modelling",
+                machine_type="n1-standard-8",
             ),
         ]
     )
 
 
-def test_get_argo_dag(pipeline: Pipeline):
+def test_get_argo_dag(pipeline: Pipeline, machine_types: dict[str, MachineType], default_machine_type: str):
 
     # When generating the argo DAG
-    argo_dag = get_argo_dag(pipeline)
+    argo_dag = get_argo_dag(pipeline, machine_types, default_machine_type)
     expected = {
         "preprocess_fun": { 
             "name": "preprocess-fun",
             "nodes": "preprocess_fun",
             "deps": [],
+            "mem": 16,
+            "cpu": 4,
+            "num_gpu": 0,
         },
         "train_fun": {
             "name": "train-fun",
             "nodes": "train_fun",
             "deps": ["preprocess-fun"],
+            "mem": 16,
+            "cpu": 8,
+            "num_gpu": 0,
         }
     }
 
@@ -121,20 +143,26 @@ def test_get_argo_dag(pipeline: Pipeline):
     assert {key: task.to_dict() for key,task in argo_dag.items()} == expected
 
 
-def test_get_argo_dag_fused(fused_pipeline: Pipeline):
+def test_get_argo_dag_fused(fused_pipeline: Pipeline, machine_types: dict[str, MachineType], default_machine_type: str):
 
     # When generating the argo DAG
-    argo_dag = get_argo_dag(fused_pipeline)
+    argo_dag = get_argo_dag(fused_pipeline, machine_types, default_machine_type)
     expected = {
         "preprocess_fun": { 
             "name": "preprocess-fun",
             "nodes": "preprocess_fun",
             "deps": [],
+            "mem": 16,
+            "cpu": 2,
+            "num_gpu": 0,
         },
         "fused_modelling": {
             "name": "fused-modelling",
             "nodes": "fused_modelling",
             "deps": ["preprocess-fun"],
+            "mem": 16,
+            "cpu": 8,
+            "num_gpu": 0,
         }
     }
 
@@ -142,25 +170,34 @@ def test_get_argo_dag_fused(fused_pipeline: Pipeline):
     assert {key: task.to_dict() for key,task in argo_dag.items()} == expected
     
 
-def test_get_argo_dag_fused_complex(fused_pipeline_complex: Pipeline):
+def test_get_argo_dag_fused_complex(fused_pipeline_complex: Pipeline, machine_types: dict[str, MachineType], default_machine_type: str):
 
     # When generating the argo DAG
-    argo_dag = get_argo_dag(fused_pipeline_complex)
+    argo_dag = get_argo_dag(fused_pipeline_complex, machine_types, default_machine_type)
     expected = {
         "preprocess_fun": { 
             "name": "preprocess-fun",
             "nodes": "preprocess_fun",
             "deps": [],
+            "mem": 16,
+            "cpu": 2,
+            "num_gpu": 0,
         },
         "preprocess_customers": { 
             "name": "preprocess-customers",
             "nodes": "preprocess_customers",
             "deps": [],
+            "mem": 16,
+            "cpu": 2,
+            "num_gpu": 0,
         },
         "fused_modelling": {
             "name": "fused-modelling",
             "nodes": "fused_modelling",
             "deps": ["preprocess-customers", "preprocess-fun"],
+            "mem": 16,
+            "cpu": 8,
+            "num_gpu": 0,
         }
     }
 
