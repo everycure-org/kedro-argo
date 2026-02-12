542eef6d8aefd9953d5e58785421662a7d25770f
Alexei Stepanenko <alexei.stepa@gmail.com>
2026-02-12 22:21:44 +0000

    fix image variable name

--- argo-kedro/argo_kedro/framework/cli/cli.py
+++ argo-kedro/argo_kedro/framework/cli/cli.py
@@ -398,7 +398,7 @@ def submit(
         context = session.load_context()
         
         # Build and push the image
-        image = f"{context.argo.deployment.image}:{context.argo.deployment.tag}"
+        full_image = f"{context.argo.deployment.image}:{context.argo.deployment.tag}"
         if not dry_run:
             publish_image(
                 full_image=full_image,
