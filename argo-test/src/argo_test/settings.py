f32394639a9cdb8ae3dddf639c70faf7fff19077
Laurens Vijnck <laurens@everycure.org>
2026-03-10 10:51:06 +0000

    rm hardcoded image

--- argo-test/src/argo_test/settings.py
+++ argo-test/src/argo_test/settings.py
@@ -27,6 +27,9 @@ https://docs.kedro.org/en/stable/kedro_project_setup/settings.html."""
 from kedro.config import OmegaConfigLoader  # noqa: E402
 from omegaconf.resolvers import oc
 
+from dotenv import load_dotenv
+load_dotenv()
+
 CONFIG_LOADER_CLASS = OmegaConfigLoader
 # Keyword arguments to pass to the `CONFIG_LOADER_CLASS` constructor.
 CONFIG_LOADER_ARGS = {
